97e74f6da0bffc550813ccc5835cdb3a92d5943c
Zsolt Marta <zsolt.marta@bitrise.io>
2026-01-15 13:48:26 +0100

    fix: ignore build dir

--- app/build/reports/paparazzi/debug/runs/20260115132922_6f8a6f.js
+++
@@ -1,8 +0,0 @@
-window.runs["20260115132922_6f8a6f"] = [
-  {
-    "testName": "com.example.paparazzitest.ContextInspectionTest#printAllContextInformation",
-    "timestamp": "2026-01-15T12:29:24.492Z",
-    "tags": [],
-    "file": "images/d30a54b981c8dced2f8beaefde20fa8b57a6a839.png"
-  }
-];
